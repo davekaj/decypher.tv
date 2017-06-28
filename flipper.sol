e27b35495780de127ced829dfca0c3ce46c52e71
EC2 Default User <ec2-user@ip-172-31-25-233.us-west-2.compute.internal>
2017-06-28 13:14:08 +0000

    first-contracts

---
+++ flipper.sol
@@ -0,0 +1,31 @@
+
+contract Flipper {
+
+  enum GameState {noWager, wagerMade, wagerAccepted}
+  GameState public currentState;
+
+  modifier onlyState(GameState expectedState) { if(expectedState == currentState) { _; } else { throw; } }
+
+  function Flipper() {
+    currentState = GameState.noWager;
+  }
+
+  function makeWager() onlyState(GameState.noWager) returns (bool) {
+    // ...
+    currentState = GameState.wagerMade;
+    return true;
+  }
+
+  function acceptWager() onlyState(GameState.wagerMade) returns (bool) {
+    // ...
+    currentState = GameState.wagerAccepted;
+    return true;
+  }
+
+  function resolveBet() onlyState(GameState.wagerAccepted) returns (bool) {
+    // ...
+    currentState = GameState.noWager;
+    return true;
+  }
+
+}
